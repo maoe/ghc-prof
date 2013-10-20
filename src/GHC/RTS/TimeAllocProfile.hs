b3014a7b369d0512539de4bbeaac5f12b3bbbd44
Mitsutoshi Aoe <maoe@foldr.in>
2013-10-20 12:38:26 +0900

    Export types and parser from GHC.RTS.TimeAllocProfile

--- src/GHC/RTS/TimeAllocProfile.hs
+++ src/GHC/RTS/TimeAllocProfile.hs
@@ -1,1 +1,6 @@
-module GHC.RTS.TimeAllocProfile where
+module GHC.RTS.TimeAllocProfile
+  ( module GHC.RTS.TimeAllocProfile.Types
+  , module GHC.RTS.TimeAllocProfile.Parser
+  ) where
+import GHC.RTS.TimeAllocProfile.Parser (timeAllocProfile)
+import GHC.RTS.TimeAllocProfile.Types
