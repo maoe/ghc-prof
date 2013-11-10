1256dbf277979f61cddd5ecf0077886657a11dcf
Mitsutoshi Aoe <maoe@foldr.in>
2013-11-10 11:19:55 +0900

    Delete GHC.RTS.TimeAllocProfile.Internal

--- src/GHC/RTS/TimeAllocProfile/Internal.hs
+++
@@ -1,7 +0,0 @@
-module GHC.RTS.TimeAllocProfile.Internal
-  ( module GHC.RTS.TimeAllocProfile.Types
-  , module GHC.RTS.TimeAllocProfile.Parser
-  ) where
-
-import GHC.RTS.TimeAllocProfile.Parser
-import GHC.RTS.TimeAllocProfile.Types
