93a7dadbe63b009935ae1b1384e49a531aac5d96
Mitsutoshi Aoe <maoe@foldr.in>
2016-12-11 00:22:27 +0900

    Add the decode function

--- src/GHC/Prof.hs
+++ src/GHC/Prof.hs
@@ -1,5 +1,6 @@
 module GHC.Prof
-  ( Profile(..)
+  ( decode
+  , Profile(..)
   , TotalTime(..)
   , TotalAlloc(..)
   , AggregateCostCentre(..)
@@ -21,6 +22,15 @@ module GHC.Prof
   , callSitesOrderBy
   ) where
 
+import qualified Data.Attoparsec.Text.Lazy as ATL
+import qualified Data.Text.Lazy as TL
+
 import GHC.Prof.CostCentreTree
 import GHC.Prof.Parser (profile)
 import GHC.Prof.Types
+
+-- | Decode a GHC time allocation profiling report from a lazy 'ATL.Text'
+decode :: TL.Text -> Either String Profile
+decode text = case ATL.parse profile text of
+  ATL.Fail _unconsumed _contexts reason -> Left reason
+  ATL.Done _unconsumed prof -> Right prof
