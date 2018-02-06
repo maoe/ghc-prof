94ae6880c22f4312f8d3d58dc331b9f0b02a32ae
Ignat Insarov <kindaro@gmail.com>
2018-02-06 11:01:11 +0700

    Add a strict parser.

--- src/GHC/Prof.hs
+++ src/GHC/Prof.hs
@@ -1,5 +1,6 @@
 module GHC.Prof
   ( decode
+  , decode'
 
   -- * Parser
   , profile
@@ -29,7 +30,9 @@ module GHC.Prof
   ) where
 
 import qualified Data.Attoparsec.Text.Lazy as ATL
+import qualified Data.Attoparsec.Text as ATS
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text as TS
 
 import GHC.Prof.CostCentreTree
 import GHC.Prof.Parser (profile)
@@ -40,3 +43,7 @@ decode :: TL.Text -> Either String Profile
 decode text = case ATL.parse profile text of
   ATL.Fail _unconsumed _contexts reason -> Left reason
   ATL.Done _unconsumed prof -> Right prof
+
+-- | Decode a GHC time allocation profiling report from a strict 'ATS.Text'
+decode' :: TS.Text -> Either String Profile
+decode' text = ATS.parseOnly (profile <* ATS.endOfInput) text
